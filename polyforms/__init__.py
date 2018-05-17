0829f96400f7b953f8a1d2cc75b27cae25ae4c49
Max Z <max.zlotskiy@gmail.com>
2018-05-17 14:31:50 -0400

    added methods under route decorators

--- polyforms/__init__.py
+++ polyforms/__init__.py
@@ -1,22 +1,35 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
+import os
 
 polyforms = Flask(__name__)
+DIR = os.path.dirname(__file__) + "/"
 
 @polyforms.route('/')
-
+def root():
+    return ""
 
 @polyforms.route('/login')
+def login_page():
     return render_template('login.html')
 
-@polyforms.route('/form')
+@polyforms.route('/form/respond', methods=["GET"])
+def display_form():
     return render_template('form.html')
 
 @polyforms.route('/ajax')
-    return redirect(url_for('/form', ))
+def ajax():
+    return redirect(url_for('/form'))
 
 @polyforms.route('/my/forms')
-
+def my_forms():
+    return ""
 
 @polyforms.route('/my/settings')
-    
+def settings_page():
+    return ""
+
+#Will not be executed if this is imported by WSGI
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
 
