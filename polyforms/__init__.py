5c60cfef9f906d838efaa42de54a91044c924d67
Max Z <max.zlotskiy@gmail.com>
2018-05-19 15:02:22 -0400

    made a temp home page; hardcoded login works; base template has 2 navbars depending on if ur logged in

--- polyforms/__init__.py
+++ polyforms/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, session, redirect, url_for, f
 import os
 
 polyforms = Flask(__name__)
+polyforms.secret_key = os.urandom(32)
 DIR = os.path.dirname(__file__) + "/"
 
 @polyforms.route('/')
@@ -13,10 +14,17 @@ def root():
     body+= '<img src="' + url_for('static', filename='img/cat_cage.jpg') + '" width="500"</img>'
     return body
 
+#This will become the root "/" page but not right now because of the deploytest stuff
+@polyforms.route('/home')
+def home_page():
+    return render_template("index.html", username=session.get("user", ""))
+
+#Shows the form to login
 @polyforms.route('/login')
 def login_page():
-    return render_template('login.html')
+    return render_template('login.html', username=session.get("user", ""))
 
+#Verifies username and password, redirects home on success
 @polyforms.route('/login/verify', methods=["POST"])
 def login_logic():
     uname = request.form.get("username", "")
@@ -25,7 +33,18 @@ def login_logic():
         session["user"] = uname
     else:
         flash("Wrong")
-    return redirect(url_for("/"))
+    return redirect(url_for("home_page"))
+
+#Shows the form to signup
+@polyforms.route('/join')
+def signup_page():
+    return "Go back this page doesnt exist yet"
+
+#Verifies that this account can be made, redirects to login page on success
+@polyforms.route('/join/verify', methods=['POST'])
+def signup_logic():
+    #insert logic to check that this account is valid
+    return redirect(url_for("home_page"))
 
 @polyforms.route('/form/respond', methods=["GET"])
 def display_form():
@@ -43,8 +62,14 @@ def my_forms():
 def settings_page():
     return ""
 
+@polyforms.route('/logout')
+def logout():
+    if "user" in session:
+        session.pop("user")
+    return redirect(url_for("home_page"))
+
 #Will not be executed if this is imported by WSGI
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    polyforms.debug = True
+    polyforms.run()
 
