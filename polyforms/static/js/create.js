98d830678d6df3ab75d5e7e51d32028ac6ee78ad
andrewWong21 <awong21@stuy.edu>
2018-06-06 14:51:18 -0400

    changed words

--- polyforms/static/js/create.js
+++ polyforms/static/js/create.js
@@ -1,6 +1,6 @@
 var q0 = document.getElementById('q0');
 var q1 = document.getElementById('q1');
-var closeButtons = document.getElementsByClassName('close');
+var closeButtons = document.getElementsByClassName('remove');
 var questions = document.getElementsByClassName('question');
 var addButton = document.getElementById('addButton');
 
@@ -20,13 +20,16 @@ var addQuestion= function(e) {
 
 addButton.addEventListener('click', addQuestion);
 
-for (var i=0; i<closeButtons.length; i++){
-	closeButtons[i].addEventListener('click', rmItem);
+for (var i=0; i<removeButtons.length; i++){
+	removeButtons[i].addEventListener('click', rmItem);
 };
 
 for (var i=0; i<questions.length; i++){
+    // highlight questions on mouseover
     questions[i].addEventListener('mouseover',function(){ this.setAttribute("style","outline: 2px dashed black; padding:5px;");});
     questions[i].addEventListener('mouseout', function(){ this.setAttribute("style","outline: 1px solid black; padding:5px;");});
+
+
 };
 
 console.log(questions);
