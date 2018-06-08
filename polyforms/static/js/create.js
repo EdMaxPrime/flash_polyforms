c5be36bc7d9e1a799f19ecdeff213247b03886ac
andrewWong21 <awong21@stuy.edu>
2018-06-08 14:46:15 -0400

    added option to make question required

--- polyforms/static/js/create.js
+++ polyforms/static/js/create.js
@@ -1,6 +1,6 @@
 var q0 = document.getElementById('q0');
 var q1 = document.getElementById('q1');
-var closeButtons = document.getElementsByClassName('remove');
+var removeButtons = document.getElementsByClassName('remove');
 var questions = document.getElementsByClassName('question');
 var addButton = document.getElementById('addButton');
 
@@ -12,10 +12,15 @@ var changeHeading = function(e) {
 };
 
 var rmItem = function(e) {
+    // removes div and list item
     this.parentElement.parentElement.remove();
 };
 
 var addQuestion= function(e) {
+    var newQuestion = document.createElement('div');
+    var node = document.lastChild;
+    newQuestion.setAttribute('style','outline: 2px dashed black; padding:5px;');
+    addButton.insertBefore(newQuestion, node);
 };
 
 addButton.addEventListener('click', addQuestion);
@@ -28,7 +33,6 @@ for (var i=0; i<questions.length; i++){
     // highlight questions on mouseover
     questions[i].addEventListener('mouseover',function(){ this.setAttribute("style","outline: 2px dashed black; padding:5px;");});
     questions[i].addEventListener('mouseout', function(){ this.setAttribute("style","outline: 1px solid black; padding:5px;");});
-
 
 };
 
