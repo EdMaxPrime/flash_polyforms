dc343569e6f3a9537a0d3db08fe515330b2383bf
Andrew <awong21@stuy.edu>
2018-06-10 22:18:13 -0400

    dynamic answerDiv working, need to add MC answer selections and fix removeQuestion

--- polyforms/static/js/create.js
+++ polyforms/static/js/create.js
@@ -1,11 +1,21 @@
 var questionList = document.getElementById('questionList');
 var removeButtons = document.getElementsByClassName('remove');
 var questions = document.getElementsByClassName('question');
+var answerDivs = document.getElementsByClassName('answer');
+var dropdowns = document.getElementsByClassName('select');
 var addButton = document.getElementById('addButton');
 
 var removeQuestion = function(e) {
     // removes list item containing the parent div
-    this.parentElement.parentElement.remove();
+    
+    var parentDiv = this.parentElement.parentElement;
+    var index = this.parentElement.getAttribute('name');
+    parentDiv.remove();
+    console.log(index);
+    // decrements all the following question numbers
+    for (var i = index; index < questionList.length; i++){
+        // reset all names
+    };
 };
 
 var addQuestion= function(e) {
@@ -27,8 +37,10 @@ var addQuestion= function(e) {
     newRemove.setAttribute('class', 'remove');
     newRemove.setAttribute('style', 'position: relative; top:0; float: right;');
     newRemove.innerHTML = 'X';
-    //newRemove.addEventListener('click', removeQuestion); does not work for some reason
-    
+    // newRemove.addEventListener('click', removeQuestion); // does not work for some reason
+    for (var i=0; i<removeButtons.length; i++){
+        removeButtons[i].addEventListener('click', removeQuestion); // workaround for above eventlistener not working
+    };
     
     questionDiv.innerHTML+='Question:';
     var newQuestionBox = document.createElement('input');
@@ -39,6 +51,8 @@ var addQuestion= function(e) {
     
     questionDiv.innerHTML+='Question Type:';
     var newDropdown = document.createElement('select');
+    newDropdown.selectedIndex = -1;
+    newDropdown.setAttribute('class', 'select');
     newDropdown.setAttribute('name', questionNumber + '.type');
     var questionTypes = ['one-line response', 'extended response', 'numerical', 'multiple-choice'];
     for (var i = 0; i < questionTypes.length; i++){
@@ -53,33 +67,58 @@ var addQuestion= function(e) {
     questionDiv.innerHTML+="Required? "
     var requiredBox = document.createElement('input');
     requiredBox.setAttribute('type', 'checkbox');
+    requiredBox.setAttribute('name', questionNumber + '.required');
     questionDiv.appendChild(requiredBox);
     
     var answerDiv = document.createElement('div');
+    answerDiv.setAttribute('class', 'answer');
     questionDiv.appendChild(answerDiv);
-    if (newDropdown.value==0 || newDropdown.value==1){
-        while (answerDiv.firstChild) answerDiv.removeChild(answerDiv.firstChild);
-        answerDiv.innerHTML+='Min. characters: ';
-        var minBox = document.createElement('input');
-        minBox.setAttribute('type', 'text');
-        minBox.setAttribute('name', questionNumber + '.min');
-        minBox.setAttribute('maxlength', '4');
-        minBox.setAttribute('size', '3');
-        answerDiv.appendChild(minBox);
-        answerDiv.innerHTML+='Max. characters: ';
-        var maxBox = document.createElement('input');
-        maxBox.setAttribute('type', 'text');
-        maxBox.setAttribute('name', questionNumber + '.max');
-        maxBox.setAttribute('maxlength', '4');
-        maxBox.setAttribute('size', '3');
-        answerDiv.appendChild(maxBox);
-    } else if (newDropdown.value == 3){
-        while (answerDiv.firstChild) answerDiv.removeChild(answerDiv.firstChild);
-        answerDiv.innerHTML='beep';
-    } else {while (answerDiv.firstChild) answerDiv.removeChild(answerDiv.firstChild);};
-
-    for (var i=0; i<removeButtons.length; i++){
-        removeButtons[i].addEventListener('click', removeQuestion); // workaround for above eventlistener not working
+    
+    // dynamic answer selection based on question type
+    for (var i = 0; i<dropdowns.length; i++){
+        dropdown = dropdowns[i];
+        dropdown.onchange = function() {
+            if (dropdown.value=="0" || dropdown.value=="1"){ // text responses
+                while (answerDiv.firstChild) answerDiv.removeChild(answerDiv.firstChild);
+                answerDiv.innerHTML+='Min. characters: ';
+                var minBox = document.createElement('input');
+                minBox.setAttribute('type', 'text');
+                minBox.setAttribute('name', questionNumber + '.min');
+                minBox.setAttribute('maxlength', '4');
+                minBox.setAttribute('size', '3');
+                answerDiv.appendChild(minBox);
+                answerDiv.innerHTML+='Max. characters: ';
+                var maxBox = document.createElement('input');
+                maxBox.setAttribute('type', 'text');
+                maxBox.setAttribute('name', questionNumber + '.max');
+                maxBox.setAttribute('maxlength', '4');
+                maxBox.setAttribute('size', '3');
+                answerDiv.appendChild(maxBox);
+            } 
+            else if (dropdown.value == "2"){ // numerical
+                while (answerDiv.firstChild) answerDiv.removeChild(answerDiv.firstChild);
+                answerDiv.innerHTML+='Min. number: ';
+                var minBox = document.createElement('input');
+                minBox.setAttribute('type', 'text');
+                minBox.setAttribute('name', questionNumber + '.min');
+                minBox.setAttribute('maxlength', '2');
+                minBox.setAttribute('size', '1');
+                answerDiv.appendChild(minBox);
+                answerDiv.innerHTML+='Max. number: ';
+                var maxBox = document.createElement('input');
+                maxBox.setAttribute('type', 'text');
+                maxBox.setAttribute('name', questionNumber + '.max');
+                maxBox.setAttribute('maxlength', '5');
+                maxBox.setAttribute('size', '3');
+                answerDiv.appendChild(maxBox);
+            } 
+            else if (dropdown.value == "3"){ // multiple-choice
+                while (answerDiv.firstChild) answerDiv.removeChild(answerDiv.firstChild);
+                answerDiv.appendChild(document.createElement('input'));
+                // add expandable list of possible choices here
+                
+            } else {while (answerDiv.firstChild) answerDiv.removeChild(answerDiv.firstChild);};
+        };
     };
 };
 
