e9fb1ba0bd97d7a5a0642a968d9f5c7d10b68944
Andrew <awong21@stuy.edu>
2018-06-11 16:35:50 -0400

    MC answers working

--- polyforms/static/js/create.js
+++ polyforms/static/js/create.js
@@ -5,17 +5,89 @@ var answerDivs = document.getElementsByClassName('answer');
 var dropdowns = document.getElementsByClassName('select');
 var addButton = document.getElementById('addButton');
 
+var dynamicAnswer = function(e){
+    // dynamic answer selection based on question type
+    for (var i = 0; i < dropdowns.length; i++){
+        console.log(dropdowns);
+        drop = dropdowns[i];
+        console.log(drop);
+        //console.log(i);
+        let index = i;
+        var dropdown = drop;
+        console.log('adding event to ' + i);
+        setTimeout(function(){
+            dropdown.onchange = function(){
+                console.log("parent is " + dropdown.parentElement.getAttribute('name'));
+                console.log('looking at ' + index);
+                answer = dropdown.parentElement.querySelector('div[class="answer"]');
+                if (dropdown.value=="0" || dropdown.value=="1"){ // text responses
+                    while (answer.firstChild) answer.removeChild(answer.firstChild);
+                    answer.innerHTML+='Min. characters: ';
+                    var minBox = document.createElement('input');
+                    minBox.setAttribute('type', 'text');
+                    minBox.setAttribute('name', index + '.min');
+                    minBox.setAttribute('maxlength', '4');
+                    minBox.setAttribute('size', '3');
+                    answer.appendChild(minBox);
+                    answer.innerHTML+='Max. characters: ';
+                    var maxBox = document.createElement('input');
+                    maxBox.setAttribute('type', 'text');
+                    maxBox.setAttribute('name', index + '.max');
+                    maxBox.setAttribute('maxlength', '4');
+                    maxBox.setAttribute('size', '3');
+                    answer.appendChild(maxBox);
+                } 
+                else if (dropdown.value == "2"){ // numerical
+                    while (answer.firstChild) answer.removeChild(answer.firstChild);
+                    answer.innerHTML+='Min. number: ';
+                    var minBox = document.createElement('input');
+                    minBox.setAttribute('type', 'text');
+                    minBox.setAttribute('name', index + '.min');
+                    minBox.setAttribute('maxlength', '2');
+                    minBox.setAttribute('size', '1');
+                    answer.appendChild(minBox);
+                    answer.innerHTML+='Max. number: ';
+                    var maxBox = document.createElement('input');
+                    maxBox.setAttribute('type', 'text');
+                    maxBox.setAttribute('name', index + '.max');
+                    maxBox.setAttribute('maxlength', '5');
+                    maxBox.setAttribute('size', '3');
+                    answer.appendChild(maxBox);
+                } 
+                else if (dropdown.value == "3"){ // multiple-choice
+                    while (answer.firstChild) answer.removeChild(answer.firstChild);
+                    answer.innerHTML+='Enter answer choices in the following box, separated by commas only';
+                    answer.appendChild(document.createElement('br'));
+                    answer.appendChild(document.createElement('input'));
+                } else {while (answer.firstChild) answer.removeChild(answer.firstChild);};
+                console.log('changed');
+            }
+        }, 1000);
+        console.log('added event to ' + i);
+    };
+};
+
 var removeQuestion = function(e) {
     // removes list item containing the parent div
-    
     var parentDiv = this.parentElement.parentElement;
     var index = this.parentElement.getAttribute('name');
     parentDiv.remove();
-    console.log(index);
+    
     // decrements all the following question numbers
-    for (var i = index; index < questionList.length; i++){
+    for (var i = parseInt(index) + 1; i < questions.length + 1; i++){
         // reset all names
+        nextDiv = document.querySelector('div[name="' + i + '"]');
+        nextDiv.setAttribute('name', i - 1);
+        var children = nextDiv.children;
+        for (var j = 0; j < children.length; j++) {
+            child = children[j];
+            if (child.getAttribute('name')){
+                child.setAttribute('name', i - 1 + child.getAttribute('name').substring(1));
+            };
+        };
     };
+    
+    dynamicAnswer(); // recalibrates event listeners for answer divs
 };
 
 var addQuestion= function(e) {
@@ -42,14 +114,7 @@ var addQuestion= function(e) {
         removeButtons[i].addEventListener('click', removeQuestion); // workaround for above eventlistener not working
     };
     
-    questionDiv.innerHTML+='Question:';
-    var newQuestionBox = document.createElement('input');
-    questionDiv.appendChild(newQuestionBox);
-    questionDiv.appendChild(document.createElement('br'));
-    newQuestionBox.setAttribute('type', 'text');
-    newQuestionBox.setAttribute('name', questionNumber + '.question');
-    
-    questionDiv.innerHTML+='Question Type:';
+    questionDiv.innerHTML+='Question Type: ';
     var newDropdown = document.createElement('select');
     newDropdown.selectedIndex = -1;
     newDropdown.setAttribute('class', 'select');
@@ -64,6 +129,13 @@ var addQuestion= function(e) {
     questionDiv.appendChild(newDropdown);
     questionDiv.appendChild(document.createElement('br'));
     
+    questionDiv.innerHTML+='Question: ';
+    var newQuestionBox = document.createElement('input');
+    questionDiv.appendChild(newQuestionBox);
+    questionDiv.appendChild(document.createElement('br'));
+    newQuestionBox.setAttribute('type', 'text');
+    newQuestionBox.setAttribute('name', questionNumber + '.question');
+    
     questionDiv.innerHTML+="Required? "
     var requiredBox = document.createElement('input');
     requiredBox.setAttribute('type', 'checkbox');
@@ -74,52 +146,8 @@ var addQuestion= function(e) {
     answerDiv.setAttribute('class', 'answer');
     questionDiv.appendChild(answerDiv);
     
-    // dynamic answer selection based on question type
-    for (var i = 0; i<dropdowns.length; i++){
-        dropdown = dropdowns[i];
-        dropdown.onchange = function() {
-            if (dropdown.value=="0" || dropdown.value=="1"){ // text responses
-                while (answerDiv.firstChild) answerDiv.removeChild(answerDiv.firstChild);
-                answerDiv.innerHTML+='Min. characters: ';
-                var minBox = document.createElement('input');
-                minBox.setAttribute('type', 'text');
-                minBox.setAttribute('name', questionNumber + '.min');
-                minBox.setAttribute('maxlength', '4');
-                minBox.setAttribute('size', '3');
-                answerDiv.appendChild(minBox);
-                answerDiv.innerHTML+='Max. characters: ';
-                var maxBox = document.createElement('input');
-                maxBox.setAttribute('type', 'text');
-                maxBox.setAttribute('name', questionNumber + '.max');
-                maxBox.setAttribute('maxlength', '4');
-                maxBox.setAttribute('size', '3');
-                answerDiv.appendChild(maxBox);
-            } 
-            else if (dropdown.value == "2"){ // numerical
-                while (answerDiv.firstChild) answerDiv.removeChild(answerDiv.firstChild);
-                answerDiv.innerHTML+='Min. number: ';
-                var minBox = document.createElement('input');
-                minBox.setAttribute('type', 'text');
-                minBox.setAttribute('name', questionNumber + '.min');
-                minBox.setAttribute('maxlength', '2');
-                minBox.setAttribute('size', '1');
-                answerDiv.appendChild(minBox);
-                answerDiv.innerHTML+='Max. number: ';
-                var maxBox = document.createElement('input');
-                maxBox.setAttribute('type', 'text');
-                maxBox.setAttribute('name', questionNumber + '.max');
-                maxBox.setAttribute('maxlength', '5');
-                maxBox.setAttribute('size', '3');
-                answerDiv.appendChild(maxBox);
-            } 
-            else if (dropdown.value == "3"){ // multiple-choice
-                while (answerDiv.firstChild) answerDiv.removeChild(answerDiv.firstChild);
-                answerDiv.appendChild(document.createElement('input'));
-                // add expandable list of possible choices here
-                
-            } else {while (answerDiv.firstChild) answerDiv.removeChild(answerDiv.firstChild);};
-        };
-    };
+    //console.log(answerDivs.length);
+    dynamicAnswer();
 };
 
 addButton.addEventListener('click', addQuestion);
@@ -133,3 +161,5 @@ for (var i=0; i<questions.length; i++){
     questions[i].addEventListener('mouseover',function(){ this.setAttribute("style","outline: 2px dashed black; padding: 5px; margin-bottom: 15px;");});
     questions[i].addEventListener('mouseout', function(){ this.setAttribute("style","outline: 1px solid black; padding: 5px; margin-bottom: 15px;");});
 };
+
+dynamicAnswer();
